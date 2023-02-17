2a893c61fe8ce4410a2e120cd523f5475920e6a8
Logan <page.lg@gmail.com>
2023-02-17 13:31:48 +0200

    chore: update @ngrx to v13

--- src/app/store/selectors/visualization.selectors.ts
+++ src/app/store/selectors/visualization.selectors.ts
@@ -4,7 +4,7 @@ import { NbtutorState, VisualizationReducer } from '../reducers';
 type State = VisualizationReducer.State;
 const selectors = VisualizationReducer.adapter.getSelectors();
 
-export const selectVisualizationState = createFeatureSelector<NbtutorState, State>(VisualizationReducer.stateKey);
+export const selectVisualizationState = createFeatureSelector< State>(VisualizationReducer.stateKey);
 
 export const selectVisualizationIds = createSelector(selectVisualizationState, selectors.selectIds);
 export const selectVisualizationEntities = createSelector(selectVisualizationState, selectors.selectEntities);
